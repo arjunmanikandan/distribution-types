832803a816f5fc558c458b8fda939f2c42280bb0
arjunmanikandan <arjunmanikandan319@gmail.com>
2024-06-19 09:41:56 +0530

    Shifted the square across the graph

--- shape.py
+++ shape.py
@@ -1,4 +1,4 @@
-#Square size can be increased or decreased using scaling factor
+#To move the square across the graph
 import matplotlib.pyplot as plt
 import sys
 import csv
@@ -10,23 +10,28 @@ def read_csv(csv_path):
         coordinates.pop(0)
     return coordinates
 
-def draw_square(coordinates,scale_value):
+def draw_square(coordinates,shift_x,shift_y):
     for coords in coordinates:
         x1, y1,side_length= int(coords[0]), int(coords[1]),int(coords[2])
-        scaling_factor = side_length*scale_value
-        x2,y2=x1+scaling_factor,y1+scaling_factor
+        x1,y1,x2,y2=x1+shift_x,y1+shift_y,x1+side_length,y1+side_length
         x = [x1, x2, x2, x1, x1]
         y = [y1, y1, y2, y2, y1]
         plt.plot(x, y, marker='o')
     plt.gca().set_aspect('equal', adjustable='box')
     plt.show()
 
+def get_user_input():
+    return {
+        "csv_path" : sys.argv[1],
+        "shift_x" : float(sys.argv[2]),
+        "shift_y":  float(sys.argv[2]) }
+
+#User Input for x and y coordinates +-x,+-y
+#python3 shape.py shape_coordinates.csv 2 3
 def main():
-    # user_input python3 shape.py shape_coordinates.csv 2
-    csv_path = sys.argv[1]
-    scale_value = float(sys.argv[2])
-    coordinates = read_csv(csv_path)    
-    draw_square(coordinates,scale_value)
+    user_inputs = get_user_input()
+    coordinates = read_csv(user_inputs["csv_path"])    
+    draw_square(coordinates,user_inputs["shift_x"],user_inputs["shift_y"])
 
 if __name__ == "__main__":
     main()
