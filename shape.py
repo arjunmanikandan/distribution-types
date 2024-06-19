be50d9682ddc0954b9c44f6eab6b38a8a5dc8c3c
arjunmanikandan <arjunmanikandan319@gmail.com>
2024-06-19 08:19:06 +0530

    Scaling Factor Introduced

--- shape.py
+++ shape.py
@@ -1,4 +1,4 @@
-#To create squares based on the coordinates provided in the CSV file
+#Square size can be increased or decreased using scaling factor
 import matplotlib.pyplot as plt
 import sys
 import csv
@@ -10,9 +10,11 @@ def read_csv(csv_path):
         coordinates.pop(0)
     return coordinates
 
-def draw_square(coordinates):
+def draw_square(coordinates,scale_value):
     for coords in coordinates:
-        x1, y1, x2, y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
+        x1, y1,side_length= int(coords[0]), int(coords[1]),int(coords[2])
+        scaling_factor = side_length*scale_value
+        x2,y2=x1+scaling_factor,y1+scaling_factor
         x = [x1, x2, x2, x1, x1]
         y = [y1, y1, y2, y2, y1]
         plt.plot(x, y, marker='o')
@@ -20,10 +22,11 @@ def draw_square(coordinates):
     plt.show()
 
 def main():
-    # user_input python3 shape.py shape_coordinates.csv
+    # user_input python3 shape.py shape_coordinates.csv 2
     csv_path = sys.argv[1]
+    scale_value = float(sys.argv[2])
     coordinates = read_csv(csv_path)    
-    draw_square(coordinates)
+    draw_square(coordinates,scale_value)
 
 if __name__ == "__main__":
     main()
